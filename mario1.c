97f16c370f8dfb2f2e4a17d65a6c02f9c963f965
Aroldo <106533710+Aroldobj@users.noreply.github.com>
2022-07-09 12:45:05 -0300

    Add files via upload

---
+++ mario1.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_int(void);
+
+int main(void)
+{
+     int i = get_positive_int();
+     for (int l = 0; l <= i; l++ )
+     {
+     for(int c = i; c > l; c--)
+     {
+          printf(" ");
+     }
+     for (int h = 0; h < l; h++)
+     {
+          printf("#");
+     }
+     printf(" ");
+     for (int h = 0; h < l; h++)
+     {
+          printf(" ");
+     }
+     printf("\n");
+     }
+}
+
+
+int get_positive_int(void)
+{
+     int n;
+     do
+     {
+          n = get_int("Digite o numero da altura: ");
+     }
+     while(n < 1 || n > 8);
+     return n;
+}
+
+
+
+
+
+
+
+
